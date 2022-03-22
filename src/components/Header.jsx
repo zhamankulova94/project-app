6ce74bf9c599bddc17a5afb04e07a49eff3ae6e1
Alua <zhamankulova94@gmail.com>
2022-03-22 15:50:33 +0600

    files added

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+ return(
+  <header className='header'>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/2560px-Playstation_logo_colour.svg.png' />
+  </header>
+ )
+}
+
+export default Header
